7c405b0dc66d39c7b98bd80533e626cb78fdede0
Matt Blanke <mattblanke@ucsb.edu>
2025-10-07 19:07:10 -0700

    xy - Added tests checking equality and tests checking hashCode

--- src/test/java/edu/ucsb/cs156/spring/hello/TeamTest.java
+++ src/test/java/edu/ucsb/cs156/spring/hello/TeamTest.java
@@ -23,4 +23,64 @@ public class TeamTest {
     // TODO: Add additional tests as needed to get to 100% jacoco line coverage, and
     // 100% mutation coverage (all mutants timed out or killed)
 
+
+    @Test
+    public void toString_returns_correct_string() {
+        assertEquals("Team(name=test-team, members=[])", team.toString());
+    }
+
+    @Test
+    public void equals_same_object_returns_true() {
+        assertEquals(true, team.equals(team));
+    }
+
+    @Test
+    public void equals_different_class_returns_false() {
+        String notATeam = "not a team";
+        assertEquals(false, team.equals(notATeam));
+    }
+
+    @Test
+    public void equals_same_name_and_members_returns_true() {
+        Team other = new Team("test-team");
+        assertEquals(true, team.equals(other));
+    }
+
+    @Test
+    public void equals_same_name_different_members_returns_false() {
+        Team other = new Team("test-team");
+        other.addMember("Alice");
+        assertEquals(false, team.equals(other));
+    }
+
+    @Test
+    public void equals_different_name_same_members_returns_false() {
+        Team other = new Team("other-team");
+        assertEquals(false, team.equals(other));
+    }
+
+    @Test
+    public void equals_different_name_and_members_returns_false() {
+        Team other = new Team("other-team");
+        other.addMember("Bob");
+        assertEquals(false, team.equals(other));
+    }
+
+    @Test
+    public void hashCode_same_content_same_hash() {
+        Team t1 = new Team();
+        t1.setName("foo");
+        t1.addMember("bar");
+        Team t2 = new Team();
+        t2.setName("foo");
+        t2.addMember("bar");
+        assertEquals(t1.hashCode(), t2.hashCode());
+    }
+
+    @Test
+    public void hashCode_default_team_value() {
+        Team t = new Team();
+        int expected = t.getName().hashCode() | t.getMembers().hashCode(); // implementation-specific
+        assertEquals(expected, t.hashCode());
+    }
 }
